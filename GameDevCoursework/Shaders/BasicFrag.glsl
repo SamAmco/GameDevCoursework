5d2e941aece860ca21b16434dcd5259f8a0a4492
SamAmco <samuel.amco@btinternet.com>
2015-02-04 02:22:20 +0000

    I got a fucking cube working!

--- GameDevCoursework/Shaders/BasicFrag.glsl
+++ GameDevCoursework/Shaders/BasicFrag.glsl
@@ -12,5 +12,5 @@ out vec4 gl_FragColor;
 
 void main(void)	
 {	
-	gl_FragColor = texture2D(tex1, IN.texCoord);//IN.colour;
+	gl_FragColor = vec4(1,1,1,1);//texture2D(tex1, IN.texCoord);//IN.colour;
 }
--- GameDevCoursework/main.cpp
+++ GameDevCoursework/main.cpp
@@ -17,7 +17,13 @@
 int _tmain(int argc, _TCHAR* argv[])
 {
 	// create the window
-	Renderer renderer(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, sf::ContextSettings(32));
+	sf::ContextSettings settings;
+	settings.depthBits = 24;
+	settings.stencilBits = 8;
+	settings.antialiasingLevel = 0;
+	settings.majorVersion = 3;
+	settings.minorVersion = 0;
+	Renderer renderer(sf::VideoMode(800, 600), "OpenGL", sf::Style::Default, settings);
 	renderer.setVerticalSyncEnabled(true);
 
 	if (glewInit() != GLEW_OK) 
@@ -29,13 +35,13 @@ int _tmain(int argc, _TCHAR* argv[])
 	Mesh*	m = Mesh::LoadMeshFile("cube.asciimesh");
 	vector<Shader*> shaders = vector<Shader*>(8);
 	shaders[0] = new Shader("Shaders/BasicVert.glsl", "Shaders/BasicFrag.glsl");
-	shaders[1] = new Shader("Shaders/SinExpandVert.glsl", "Shaders/BasicFrag.glsl");//A vertex shader which modulates the scale of objects over time, using a uniform variable.
-	shaders[2] = new Shader("Shaders/BasicVert.glsl", "Shaders/TexBlendSineFrag.glsl");//A fragment shader which blends back and forth between two textures.
-	shaders[3] = new Shader("Shaders/BasicVert.glsl", "Shaders/BasicFrag.glsl", "Shaders/PosSineGeom.glsl");//A geometry shader using a position-based sine wave to move vertices.
-	shaders[4] = new Shader("Shaders/BasicVert.glsl", "Shaders/ThreeTexBlendFrag.glsl");//Perform a non-trivial blend between two textures, perhaps using a third, greyscale texture to determine blend levels.
-	shaders[5] = new Shader("Shaders/BasicVert.glsl", "Shaders/BasicFrag.glsl", "Shaders/WireframeGeom.glsl");//Take in the vertices of the cube, and output new primitives from them (points, lines, or triangles forming new cubes)
-	shaders[6] = new Shader("Shaders/PhongVert.glsl", "Shaders/PhongFrag.glsl");//Calculate per - fragment lighting(using light information accessed from uniform variables)
-	shaders[7] = new Shader("Shaders/BasicVert.glsl", "Shaders/TransparentWhiteFrag.glsl");//Make part of the cube semi-transparent, while still rendering correctly from all orientations
+	//shaders[1] = new Shader("Shaders/SinExpandVert.glsl", "Shaders/BasicFrag.glsl");//A vertex shader which modulates the scale of objects over time, using a uniform variable.
+	//shaders[2] = new Shader("Shaders/BasicVert.glsl", "Shaders/TexBlendSineFrag.glsl");//A fragment shader which blends back and forth between two textures.
+	//shaders[3] = new Shader("Shaders/BasicVert.glsl", "Shaders/BasicFrag.glsl", "Shaders/PosSineGeom.glsl");//A geometry shader using a position-based sine wave to move vertices.
+	//shaders[4] = new Shader("Shaders/BasicVert.glsl", "Shaders/ThreeTexBlendFrag.glsl");//Perform a non-trivial blend between two textures, perhaps using a third, greyscale texture to determine blend levels.
+	//shaders[5] = new Shader("Shaders/BasicVert.glsl", "Shaders/BasicFrag.glsl", "Shaders/WireframeGeom.glsl");//Take in the vertices of the cube, and output new primitives from them (points, lines, or triangles forming new cubes)
+	//shaders[6] = new Shader("Shaders/PhongVert.glsl", "Shaders/PhongFrag.glsl");//Calculate per - fragment lighting(using light information accessed from uniform variables)
+	//shaders[7] = new Shader("Shaders/BasicVert.glsl", "Shaders/TransparentWhiteFrag.glsl");//Make part of the cube semi-transparent, while still rendering correctly from all orientations
 	int shaderInd = 0;
 
 
@@ -58,27 +64,28 @@ int _tmain(int argc, _TCHAR* argv[])
 	while (running)
 	{
 		sf::Event event;
-		while (renderer.pollEvent(event))
+		if (renderer.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				running = false;
 			else if (event.type == sf::Event::Resized)
 				glViewport(0, 0, event.size.width, event.size.height);
-
-			//float msec = renderer.GetTimer()->GetTimedMS();
-
-			//Matrix4 p = Matrix4();
-			//p.ToIdentity();
-			//p.SetPositionVector(lightPos);
-			//lightPos = (Matrix4::Rotation(0.1f * msec, Vector3(0, 1, 0)) * p).GetPositionVector();
-			//renderer.SetMainLight(lightCol, lightPos, lightRad);
-			o.SetModelMatrix(o.GetModelMatrix() * Matrix4::Rotation(0.1f * 100, Vector3(1, 1, 1)));
-
-			renderer.UpdateScene(100);
-			renderer.clear();
-			renderer.RenderScene();
-			renderer.display();
 		}
+
+		//float msec = renderer.GetTimer()->GetTimedMS();
+
+		//Matrix4 p = Matrix4();
+		//p.ToIdentity();
+		//p.SetPositionVector(lightPos);
+		//lightPos = (Matrix4::Rotation(0.1f * msec, Vector3(0, 1, 0)) * p).GetPositionVector();
+		//renderer.SetMainLight(lightCol, lightPos, lightRad);
+		o.SetModelMatrix(o.GetModelMatrix() * Matrix4::Rotation(0.1f * 100, Vector3(1, 1, 1)));
+
+		renderer.UpdateScene(100);
+		//renderer.clear();
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		renderer.RenderScene();
+		renderer.display();
 	}
 	delete m;
 	for (unsigned int i = 0; i < shaders.size(); ++i)
